228992320a56d63cdc3a5a7a208bd5866cc562b0
ENTRAR <ENTRAR@PC-1200>
2014-11-04 12:04:15 +0000

    Bonito

---
+++ src/Ordenar1Vector.java
@@ -0,0 +1,14 @@
+
+public class Ordenar1Vector implements OrdenarVector {
+
+    @Override
+    public String nombreMetodo() {
+        return "ordena";
+    }
+
+    @Override
+    public void ordena(int[] v, DatosEstadisticos de) {
+        
+    }
+    
+}
